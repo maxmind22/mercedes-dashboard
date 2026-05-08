006b12c5c4dc597e9aefea89bf9a0397c9f7bcae
maxmind22 <dogmax3@gmail.com>
2026-05-08 15:10:22 +0200

    Removed test files and untracked .vscode

--- test_coredump.cpp
+++
@@ -1,3 +0,0 @@
-#include "esp_core_dump.h"
-void setup() {}
-void loop() {}
--- test_reset.cpp
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-#include <esp_system.h>
-void setup() {
-    esp_reset_reason_t reason = esp_reset_reason();
-    if(reason == ESP_RST_PANIC) {
-        // Crash happened
-    }
-}
-void loop() {}
